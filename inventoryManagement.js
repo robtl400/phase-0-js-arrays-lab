ea4a83801da6a74dcaf98ccef8bb2f5ac88848b9
Rob Lord <robertlord@outlook.com>
2025-11-08 15:28:40 -0800

    completed lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,22 @@
 // Write your code here
 
+const products = ["Laptop", "Phone", "Headphones", "Monitor"]
 
+function logFirstProduct() {
+  console.log(products[0])
+}
+
+function addProduct(productName) {
+  products.push(productName)
+}
+
+function updateProductName(position, newName) {
+  products[position] = newName
+}
+
+function removeLastProduct() {
+  products.pop()
+}``
 
 // Export the necessary parts for testing
 module.exports = {
